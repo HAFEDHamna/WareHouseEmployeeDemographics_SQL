8536178de2e31a055228a9c348c940c328a15134
HAFEDHamna <123740773+HAFEDHamna@users.noreply.github.com>
2023-08-04 20:36:49 +0200

    Add files via upload

---
+++ WareHouseEmployeeDemographics_SQL.sql
@@ -0,0 +1,61 @@
+Create Table WareHouseEmployeeDemographics 
+(EmployeeID int, 
+FirstName varchar(50), 
+LastName varchar(50), 
+Age int, 
+Gender varchar(50)
+)
+SELECT * 
+FROM WareHouseEmployeeDemographics
+Insert into WareHouseEmployeeDemographics VALUES
+(1013, 'Darryl', 'Philbin', NULL, 'Male'),
+(1050, 'Roy', 'Anderson', 31, 'Male'),
+(1051, 'Hidetoshi', 'Hasagawa', 40, 'Male'),
+(1052, 'Val', 'Johnson', 31, 'Female')
+SELECT *
+FROM SQLTutorial.dbo.WareHouseEmployeeDemographics
+SELECT * 
+FROM SQLTutorial.dbo.EmployeeDemographics
+Full Outer Join SQLTutorial.dbo.WareHouseEmployeeDemographics
+     ON EmployeeDemographics.EmployeeID = wareHouseEmployeeDemographics.EmployeeID
+SELECT * 
+FROM SQLTutorial.dbo.EmployeeDemographics
+SELECT * 
+FROM SQLTutorial.dbo.WareHouseEmployeeDemographics
+SELECT EmployeeID, FirstName, Age
+FROM SQLTutorial.dbo.EmployeeDemographics
+UNION
+SELECT EmployeeID, JobTitle, Salary
+FROM SQLTutorial.dbo.EmployeeSalary
+ORDER BY EmployeeID
+SELECT FirstName, LastName, Age,
+CASE
+    WHEN Age > 30 THEN 'old'
+	ELSE 'Young'
+END
+FROM SQLTutorial.dbo.EmployeeDemographics
+WHERE Age is NOT NULL
+Order by Age
+SELECT FirstName, LastName, JobTitle, Salary,
+CASE 
+    WHEN JobTitle = 'Salesman' THEN Salary + (Salary * .10)
+	WHEN JobTitle = 'HR' THEN Salary + (Salary * .000001)
+	When JobTitle = 'Accountant' THEN Salary + (Salary * .05)
+	Else salary + (salary * .03)
+	END  AS SalaryAfterRaise
+From SQLTutorial.dbo.EmployeeDemographics
+Join SQLTutorial.dbo.EmployeeSalary
+     ON EmployeeDemographics.EmployeeID = EmployeeSalary.EmployeeID
+SELECT JobTitle, Avg(Salary) AS NewSalary
+From SQLTutorial.dbo.EmployeeDemographics
+Join SQLTutorial.dbo.EmployeeSalary
+    ON EmployeeDemographics.EmployeeID = EmployeeSalary.EmployeeID
+Group by JobTitle
+HAVING Avg(Salary) > 45000
+Order by Avg(Salary) 
+SELECT *
+FROM SQLTutorial.dbo.EmployeeDemographics
+--UPDATE SQLTutorial.dbo.EmployeeDemographics
+--SET EmployeeID = 1010 WHERE FirstName = 'Kevin' AND LastName = 'Malone'
+DELETE FROM SQLTutorial.dbo.EmployeeDemographics
+Where EmployeeID =  1010
